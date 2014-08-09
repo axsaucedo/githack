34bca4495cf0c2c60399cc105d3b3393561d1c29
Alejandro Saucedo <axsauze@gmail.com>
2014-08-09 18:26:48 +0400

    all in order

--- githack/urls.py
+++ githack/urls.py
@@ -6,16 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
-<<<<<<< HEAD
     # Home
 #    url(r'^$', 'githack.views.home', name='home'),
-
-    # API
-=======
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
->>>>>>> 7801a4311ba9a6b360342ec24dfa6789df94acec
 
     url(r'^admin/', include(admin.site.urls)),
 )
