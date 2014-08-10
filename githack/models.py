d82dc417919b5fc471966cf5795b4c63ad89a1d4
Alejandro Saucedo <axsauze@gmail.com>
2014-08-10 09:55:26 +0400

    try

--- githack/models.py
+++ githack/models.py
@@ -39,7 +39,7 @@ class Badges(models.Model):
         imgf.flush()
         tf = tempfile.NamedTemporaryFile()
         os.popen("convert %s %s" % (imgf.name,tf.name+".jpg"))
-        self.textimage = os.popen("jp2a --colors --height=10 --width=10 %s" % (tf.name+".jpg")).read()
+        self.textimage = os.popen("jp2a --colors --size=60x60 %s" % (tf.name+".jpg")).read()
         super(Badges,self).save(*args,**kwargs)
 
     def __str__(self):
