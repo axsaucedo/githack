3f87eda1fa54d860d0b428a14dc0176665595586
Alejandro Saucedo <axsauze@gmail.com>
2014-08-10 16:59:30 +0400

    push

--- githack/admin.py
+++ githack/admin.py
@@ -1,10 +1,11 @@
 from django.contrib.admin import site, ModelAdmin
-from githack.models import GitScore, Commit, Badges
+from githack.models import GitScore, Commit, Badges, Subdomain
 from accounts.models import UserProfile
 
 
 site.register(GitScore)
 site.register(Commit)
+site.register(Subdomain)
 site.register(Badges)
 
 site.register(UserProfile)
