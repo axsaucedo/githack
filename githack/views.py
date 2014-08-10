71b79a1e57b57b793710e2f94969e41460f80fae
Alejandro Saucedo <axsauze@gmail.com>
2014-08-10 14:11:49 +0400

    committed

--- githack/views.py
+++ githack/views.py
@@ -3,38 +3,51 @@ from django.contrib.auth.models import User
 import json
 
 from githack.models import Commit, Badges
+from django.contrib.auth import authenticate
+
 import os
 
 def usercommit(request):
 
-    data = json.loads(request.body)
-   
-    user = User.objects.all()[0]
-
-    obj = Commit()
-    obj.timelength =  data['inputtime']
-    obj.linesremoved = data['linesremoved']
-    obj.linesadded = data['linesadded']
-    obj.viminputsessions = data['inputsessions']
-    obj.user =user
-    obj.save()
-
-    new_levels = obj.check_for_level()
-    new_badges = obj.check_for_badges()
-
+#    This is required to use the libraries like toilet, etc
     os.environ['PATH'] += os.pathsep + '/usr/local/bin/'
 
-    text = ""
-    for badge in new_badges:
-        text = text + "\n" + badge.textimage + "\n Congratulations!! You've acquired '%s'!!\n" % badge.name
+    data = json.loads(request.body)
 
-    if new_levels['levelup']:
-        text = text + os.popen('toilet --gay LEVEL UP!').read()
+    username = data['user']
+    password = data['password']
 
-    text = text + os.popen('echo " \x1b[42;30mLevel: %i, Exp: %i / %i\033[0m"\n' % (new_levels['level'], new_levels['progress'], new_levels['totalexp'])).read()
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        # the password verified for the user
+        if user.is_active:
+            obj = Commit()
+            obj.timelength =  data['inputtime']
+            obj.linesremoved = data['linesremoved']
+            obj.linesadded = data['linesadded']
+            obj.viminputsessions = data['inputsessions']
+            obj.user =user
+            obj.save()
 
-    response = {
-        "text" : text,
-    }
+            new_levels = obj.check_for_level()
+            new_badges = obj.check_for_badges()
+
+
+            text = ""
+            for badge in new_badges:
+                text = text + "\n" + badge.textimage + "\n Congratulations!! You've acquired '%s'!!\n" % badge.name
+
+            if new_levels['levelup']:
+                text = text + os.popen('toilet --gay LEVEL UP!').read()
+
+            text = text + os.popen('echo " \x1b[42;30mLevel: %i, Exp: %i / %i\033[0m"\n' % (new_levels['level'], new_levels['progress'], new_levels['totalexp'])).read()
+
+            response = {
+                "text" : text,
+            }
+        else:
+            response = { "text" : "\x1b[42;30mYour account has been suspended - please contact the team for any questions.\033[0m", }
+    else:
+        response = { "text" : "\x1b[42;30mThe username and password provided did not match any user in our system.\033[0m", }
 
     return HttpResponse(json.dumps(response), content_type="application/json")
